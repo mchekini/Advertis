c44e2c5595d9314deecb6984c5e50fe21e7e4719
mchekini <m_chekini@esi.dz>
2017-08-07 21:14:16 +0200

    init project

---
+++ src/main/java/com/a92/AdvertisApplication.java
@@ -0,0 +1,12 @@
+package com.a92;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/a92/AdvertisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.a92;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvertisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
